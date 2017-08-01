faf4dc9f8367295fe7260f7b0528e0ad9bece47a
bu6hunt3r <Felix1Koch@gmail.com>
2017-08-01 20:24:35 +0200

    Reconf extract_vmlinux

--- extract_vmlinux.sh
+++ extract_vmlinux.sh
@@ -7,7 +7,7 @@ function usage() {
 
 if [[ $# -eq 1 ]]; then
 	#echo "Num of args: $#"
-	od -A d -t x1 $1 | grep '1f 8b 08 00'
+	[[ -e $1 ]] && od -A d -t x1 $1 | grep '1f 8b 08 00' && echo -e "Next step would be 'dd if=vmlinuz bs=1 skip=24584 | zcat > vmlinux'\n";
 else	
 	usage
 	exit
