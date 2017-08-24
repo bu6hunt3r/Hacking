3a9351cab4a0cdb62a2c1ed500619ff0c8c9a6cf
bu6hunt3r <Felix1Koch@gmail.com>
2017-08-24 18:54:09 +0200

    Added CRT, ModInv, egcd to Utils

--- Crypto/attackrsa/Utils.py
+++ Crypto/attackrsa/Utils.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python
 
+import gmpy
+
+def egcd(a,b):
+    u, u1 = 1, 0
+    v, v1 = 0, 1
+    g, g1 = a, b
+    while g1:
+            q = g // g1
+            u, u1 = u1, u - q * u1
+            v, v1 = v1, v - q * v1
+            g, g1 = g1, g - q * g1
+    return u, v    
+
+def modInv(a,m):
+    '''
+    Return r such that a*r mod m = 1
+    '''
+    return gmpy.invert(a,m)
+
+def CRT(ds, rs):
+    pass
+    
+def main():
+    print egcd(5,3)
+    print modInv(17,3)
+if __name__ == '__main__':
+    main()
