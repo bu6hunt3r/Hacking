280e903b493731bb1ce900f572114930a781548c
bu6hunt3r <Felix1Koch@gmail.com>
2017-08-27 14:44:02 +0200

    Changed ChosenCipher.py

--- Crypto/attackrsa/ChosenCipher.py
+++ Crypto/attackrsa/ChosenCipher.py
@@ -3,7 +3,12 @@
 import Utils
 
 class ChosenCipher(object):
-    def __init__(self,n,e,c):
-        self.n=n
-        self.e=e
-        self.c=c
+    def __init__(self, n, e, c):
+        self.n = n
+        self.e = e
+        self.c = c
+    def mulFactor(self, f):
+        return (self.c * pow(f, self.e, self.n))%(self.n)
+    def decrypt(self, f, newP):
+        inv = Utils.modInv(f, self.n)
+return (newP*inv)%(self.n)
