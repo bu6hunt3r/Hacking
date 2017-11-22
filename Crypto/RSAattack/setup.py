406c62dd3f8ad43b8db13cf35c8176bf291482fd
bu6hunt3r <Felix1Koch@gmail.com>
2017-11-22 13:07:12 +0100

    Removed RSAattack

--- Crypto/RSAattack/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='RSAattack',
-      version='1.0',
-      author='cr0c0',
-      description="Implementation of RSA wiener attack including fermat's factorization method",
-      py_modules=['RSAattack']
-     )
